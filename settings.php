fd4be34ca52a483c04e12430b3241864fe16f2e6
jacktonewambui <jacktoneabongo@gmail.com>
2022-10-31 17:55:39 +0300

    init

---
+++ settings.php
@@ -0,0 +1,16 @@
+<?php
+
+session_start();
+
+$con = mysqli_connect('localhost','root','');
+	
+	mysqli_select_db($con, 'warrenconcerts');
+	
+	$name = $_POST['Name'];
+	$email = $_POST['Email'];
+	$password = $_POST['Password'];
+	$contact = $_POST['Contact'];
+	$city = $_POST['City'];
+	$address = $_POST['Address'];
+
+    
