720ee3e526b747216be41e4a5e2e70c5227cc66b
jacktonewambui <jacktoneabongo@gmail.com>
2022-11-10 13:56:32 +0300

    rt

--- settings.php
+++ settings.php
@@ -1,14 +1,59 @@
 <?php
-
-session_start();
-
-$con = mysqli_connect('localhost','root','');
+ $con = mysqli_connect('localhost','root','');
 	
-	mysqli_select_db($con, 'warrenconcerts');
-	
-	$name = $_POST['Name'];
-	$email = $_POST['Email'];
-	$password = $_POST['Password'];
-	$contact = $_POST['Contact'];
-	$city = $_POST['City'];
-	$address = $_POST['Address'];
+ mysqli_select_db($con, 'warrenconcerts');
+
+if(isset($_POST['change_password'])){
+	$currentPassword = $_POST['Old Password'];
+	$password = $_POST['NewPassword'];
+	$passwordConfirm = $_POST['rtnp'];
+
+	$sql="SELECT Password from signup where Email='$email'";
+	$res = mysqli_query($con,$sql);
+
+	$row = mysqli_fetch_assoc($res);
+	if(password_verify($currentPassword,$row['Password'])){
+if($passwordConfirm ==''){
+		 $error[] = 'Please confirm the password.';
+	 }
+	 if($password != $passwordConfirm){
+		 $error[] = 'Passwords do not match.';
+	 }
+	   if(strlen($password)<5){ // min 
+		 $error[] = 'The password is 6 characters long.';
+	 }
+	 
+	  if(strlen($password)>20){ // Max 
+		 $error[] = 'Password: Max length 20 Characters Not allowed';
+	 }
+if(!isset($error))
+{
+   $options = array("cost"=>4);
+ $password = password_hash($password,PASSWORD_BCRYPT,$options);
+
+  $result = mysqli_query($con,"UPDATE signup SET password='$password' WHERE Email='$email'");
+		if($result)
+		{
+	header("location:product.html?password_updated=1");
+		}
+		else 
+		{
+		 $error[]='Something went wrong';
+		}
+}
+
+	 } 
+	 else 
+	 {
+		 $error[]='Current password does not match.'; 
+	 }   
+ }
+	 if(isset($error)){ 
+
+foreach($error as $error){ 
+echo '<p class="errmsg">'.$error.'</p>'; 
+}
+}
+	 ?> 
+    
+}
